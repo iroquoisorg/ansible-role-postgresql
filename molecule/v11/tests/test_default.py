cfcdcf185ed7b81bb2ac633d2d121c85ad6cdcb4
Norbert Orzechowicz <norbert@orzechowicz.pl>
2019-03-24 00:34:05 +0100

    Fixed failing test

--- molecule/v11/tests/test_default.py
+++ molecule/v11/tests/test_default.py
@@ -16,6 +16,3 @@ def test_postgresql_is_running(host):
     postgresql = host.process.filter(user="postgres", comm="postgres")
 
     assert len(postgresql) >= 5
-
-def test_if_postgresql_listens_on_tcp_port(host):
-    assert host.socket('tcp://127.0.0.1:5432').is_listening
